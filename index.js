60947cd1b738f05c75f218645dedf75844eaa6ad
Pritesh G <pritesh@wow.com>
2017-10-04 21:50:44 +0530

    Update index.js
    Added comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//Initializing Discord.js Listener
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log('ready');
 });
+// storing jokes in the array jokes
 
 var jokes = [
     { name: 'Dozen', answer: 'anybody want to let me in?' },
@@ -10,35 +12,17 @@ var jokes = [
     { name: 'Ice Cream', answer: 'if you don\'t let me in!' },
     { name: 'Adore', answer: 'is between us. Open up!' },
     { name: 'Lettuce', answer: 'in. Its cold out here!' },
-    { name: 'Bed', answer: 'you can not guess who I am.' },
-    { name: 'Al', answer: 'give you a kiss if you open the door.' },
-    { name: 'Olive', answer: 'you!' },
-    { name: 'Abby', answer: 'birthday to you!' },
-    { name: 'Rufus', answer: 'the most important part of your house.' },
-    { name: 'Cheese', answer: 'a cute girl.' },
-    { name: 'Wanda', answer: 'hang out with me right now?' },
-    { name: 'Ho-ho.', answer: 'You know, your Santa impression could use a little work.' },
-    { name: 'Mary and Abbey.', answer: 'Mary Christmas and Abbey New Year!' },
-    { name: 'Carmen', answer: 'let me in already!' },
-    { name: 'Ya', answer: 'I’m excited to see you too!' },
-    { name: 'Scold', answer: 'outside—let me in!' },
-    { name: 'Robin', answer: 'you! Hand over your cash!' },
-    { name: 'Irish', answer: 'you a Merry Christmas!' },
-    { name: 'Otto', answer: 'know whats taking you so long!' },
-    { name: 'Needle', answer: 'little help gettin in the door.' },
-    { name: 'Luke', answer: 'through the keyhole to see!' },
-    { name: 'Justin', answer: 'the neighborhood and thought Id come over.' },
-    { name: 'Europe', answer: 'No, you are a poo' },
-    { name: 'To', answer: 'To Whom.' },
-    { name: 'Etch', answer: 'Bless You!' },
     { name: 'Mikey', answer: 'doesnt fit through this keyhole' }
 ]
+
+//choosing a random joke from the array
 
 var knock = function() {
     var joke = jokes[Math.floor(Math.random() * jokes.length)]
     return formatJoke(joke)
 }
 
+//Formatting the output to return in a new line and plug in the output variables
 function formatJoke(joke) {
     return [
         'Knock, knock.',
@@ -49,6 +33,7 @@ function formatJoke(joke) {
     ].join('\n')
 }
 
+//Function knock() returns the formatted joke
 client.on('message', (message) => {
     if (message.content.includes('/knock')) {
         const msg = message.content.split(' ');
